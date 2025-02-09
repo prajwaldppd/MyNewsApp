e1712fbd4ad55530a3e52b54d6fc7b3a96c3c718
prajwaldppd <prajwaldppd@gmail.com>
2025-02-09 23:22:27 +0530

    added a null check

--- src/components/News.js
+++ src/components/News.js
@@ -36,7 +36,7 @@ export class News extends Component {
       <div className="container my-5">
         <h1 className="text-center">News Today - Top Headlines!</h1>
         <div className="row ">
-          {this.state.articles.map((element) => {
+          {this.state.articles && this.state.articles.map((element) => {
             return (
               <div className="col-md-4 " key={element.url}>
                 <NewsItem
